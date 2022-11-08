7b8cd290b8c1efa6c911e7a34b77d2cd437a43ad
ToasterVan-School <108506830+ToasterVan@users.noreply.github.com>
2022-11-08 13:16:22 +0100

    Simplified Script.js
    Simplified Script.js by moving onclick to html

--- Månelanding/JavaScript/Script.js
+++ Månelanding/JavaScript/Script.js
@@ -1,13 +1,5 @@
 const romferge = document.getElementById("Romferge");
 
-const buttons = {
-    start: document.getElementById("Start"),
-    stopp: document.getElementById("Stopp")
-}
-
-buttons.start.addEventListener("click", PlayAnimation);
-buttons.stopp.addEventListener("click", PauseAnimation);
-
 function PlayAnimation() {
     romferge.style.animationPlayState = 'running';
 }
