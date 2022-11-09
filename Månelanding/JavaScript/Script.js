430516a3b411814beeab73d79fa2d2c6bb9195be
ToastPi <henrygras@icloud.com>
2022-11-09 17:54:38 +0100

    Update Script.js

--- Månelanding/JavaScript/Script.js
+++ Månelanding/JavaScript/Script.js
@@ -1,7 +1,22 @@
 const romferge = document.getElementById("Romferge");
 
+let timer = setInterval(AutoLaunchAnimation, 5000);
+let animationPlaying = false;
+
+function AutoLaunchAnimation() {
+    romferge.style.animationPlayState = 'running';
+
+    animationPlaying = true;
+}
+
 function PlayAnimation() {
     romferge.style.animationPlayState = 'running';
+
+    if(!animationPlaying) {
+        clearInterval(timer);
+
+        timer = null;
+    }
 }
 
 function PauseAnimation() {
