00f12a51e5f669c6cb0ffce17854a57d39718da2
ToasterVan <108506830+ToasterVan@users.noreply.github.com>
2023-01-11 08:05:09 +0000

    Update script.ts

--- Hangman/Scripts/script.js
+++ Hangman/Scripts/script.js
@@ -1,22 +1,40 @@
-const c = console;
-const ord = ["l", "h", "e", "m", "m", "e", "l", "i", "g", "m", "m", "m", "l"];
+const ord = ["h", "e", "m", "m", "e", "l", "i", "g"];
+const hidden = [];
+const inputField = document.querySelector("input");
+const button = document.querySelector("button");
+const outputEl = document.getElementById("Output");
 function CheckLetter(array, wantedLetter) {
     let positions = [];
-    for (let letter in array) {
-        if (array[letter] == wantedLetter) {
-            positions = [...positions, letter];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == wantedLetter) {
+            positions = [...positions, i];
         }
         ;
     }
     if (positions.length != 0) {
-        const f = new Intl.ListFormat("en-us");
-        return f.format(positions);
+        return positions;
+    }
+    return "not found";
+}
+function CreateHidden(word) {
+    let hiddenArray = [];
+    for (let i in word) {
+        hiddenArray = [...hiddenArray, "_"];
+    }
+    return hiddenArray;
+}
+button.onclick = () => {
+    const letter = inputField.value;
+    const exists = CheckLetter(ord, letter);
+    if (exists == "not found") {
+        outputEl.innerHTML = "Bokstaven er ikke i ordet";
     }
     else {
-        return "not found";
+        outputEl.innerHTML = `bokstaven er i ordet på $`;
     }
-}
-c.log(`index of "l" is ${CheckLetter(ord, "l")}`);
-c.log(`index of "e" is ${CheckLetter(ord, "e")}`);
-c.log(`index of "m" is ${CheckLetter(ord, "m")}`);
-c.log(`index of "ø" is ${CheckLetter(ord, "ø")}`);
+};
+console.log(`index of "l" is ${CheckLetter(ord, "l")}`);
+console.log(`index of "e" is ${CheckLetter(ord, "e")}`);
+console.log(`index of "m" is ${CheckLetter(ord, "m")}`);
+console.log(`index of "ø" is ${CheckLetter(ord, "ø")}`);
+console.log(`\nhidden word is ${CreateHidden(ord)}`);
--- Hangman/Scripts/script.ts
+++ Hangman/Scripts/script.ts
@@ -1,23 +1,52 @@
-const c = console;
+const ord = ["h", "e", "m", "m", "e", "l", "i", "g"]
+const hidden: string[] = []
 
-const ord = ["l", "h", "e", "m", "m", "e", "l", "i", "g", "m", "m", "m", "l"]
+const inputField = document.querySelector("input");
+const button = document.querySelector("button");
 
-function CheckLetter(array: string[], wantedLetter: string): string | string[] {
-    let positions: string[] = [];
+const outputEl = document.getElementById("Output");
+
+function CheckLetter(array: string[], wantedLetter: string): string | number[] {
+    let positions: number[] = [];
     
-    for(let letter in array) {
-        if(array[letter] == wantedLetter) {
-            positions = [...positions, letter]
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] == wantedLetter) {
+            positions = [...positions, i];
         };
     }
     
     if(positions.length != 0) {
-        return new Intl.ListFormat("en-us").format(positions);
+        return positions;
     }
     return "not found";
 }
 
-c.log(`index of "l" is ${CheckLetter(ord, "l")}`);
-c.log(`index of "e" is ${CheckLetter(ord, "e")}`);
-c.log(`index of "m" is ${CheckLetter(ord, "m")}`);
-c.log(`index of "ø" is ${CheckLetter(ord, "ø")}`);
+function CreateHidden(word: string[]) {
+    let hiddenArray: string[] = []
+
+    for(let i in word) {
+        hiddenArray = [...hiddenArray, "_"];
+    }
+
+    return hiddenArray;
+}
+
+button.onclick = () => {
+    const letter = inputField.value;
+
+    const exists = CheckLetter(ord, letter);
+
+    if(exists == "not found") {
+        outputEl.innerHTML = "Bokstaven er ikke i ordet"
+    }
+    else {
+        outputEl.innerHTML = `bokstaven er i ordet på ${exists.length} ${"sted" ? exists.length == 1 : "steder"}`
+    }
+}
+
+console.log(`index of "l" is ${CheckLetter(ord, "l")}`);
+console.log(`index of "e" is ${CheckLetter(ord, "e")}`);
+console.log(`index of "m" is ${CheckLetter(ord, "m")}`);
+console.log(`index of "ø" is ${CheckLetter(ord, "ø")}`);
+
+console.log(`\nhidden word is ${CreateHidden(ord)}`);
